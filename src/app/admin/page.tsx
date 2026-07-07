3d0a93fc31bda95cbf3ce15ea395288ce8a11904
Kuma-ux <mollyjane797@gmail.com>
2026-07-08 00:06:26 +0300

    Update page.tsx

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -114,6 +114,7 @@ export default function AdminPage() {
           email: selectedUser.email,
           subject: emailSubject,
           message: emailBody,
+          html: emailBody
         }),
       }
     );
