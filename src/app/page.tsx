3626b52e7bc8a13e19b073ac5288240a04bec1a4
Kuma-ux <mollyjane797@gmail.com>
2026-08-18 22:57:06 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -928,9 +928,11 @@ export default function HomePage() {
                     {option}
                   </div>
 
-                  <div className="text-xl font-black text-emerald-400">
-                    {((item.odds?.[option] ?? 0) * 100).toFixed(0)}%
-                  </div>
+                  {!isEvent && (
+                    <div className="text-emerald-400">
+                      {((item.odds?.[option] ?? 0) * 100).toFixed(0)}%
+                    </div>
+                  )}
                 </div>
               ))
 
